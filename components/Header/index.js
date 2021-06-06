3f78edd7c2d8f5a222b8f7bb980df644a3042283
afatihyavasi <afatihyavasi@gmail.com>
2021-06-06 16:26:43 +0300

    Artist page done

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,7 +1,11 @@
+import Head from 'next/head'
 import styles from './index.module.css';
 const Header = ({ title }) => {
     return (
         <header>
+            <Head>
+                <title>{title}</title>
+            </Head>
             <h1 className={styles.header}>{title}</h1>
         </header>
     );
--- pages/artist/[slug].js
+++ pages/artist/[slug].js
@@ -1,17 +1,53 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Layout from '../../components/Layout';
+import Header from '../../components/Header';
+import styles from './index.module.css';
 const Artist = ({ data }) => {
-    console.log(data);
-    return <div></div>;
+    return (
+        <Layout>
+            {data.artists ? (
+                data.artists[0].strArtist === 'Tarkan' ? (
+                    <Link href={'https://youtu.be/AsL8sViwGgo?t=84'}>
+                        <a className={styles.aacayipsin}>
+                            Ah yanar döner aacayipsin &#8594;
+                        </a>
+                    </Link>
+                ) : (
+                    <>
+                        <Header title={data.artists[0].strArtist} />
+                        <figure className={styles.image}>
+                            <Image
+                                src={data.artists[0].strArtistThumb}
+                                alt={`${data.artists[0].strArtistThumb} thumb`}
+                                width={600}
+                                height={400}
+                                objectFit={'fill'}
+                                className={'rounded-md'}
+                            />
+                        </figure>
+                        <section>
+                            <p className={styles.biography}>
+                                {data.artists[0].strBiographyEN}
+                            </p>
+                        </section>
+                    </>
+                )
+            ) : (
+                <div>Nothing here ...</div>
+            )}
+        </Layout>
+    );
 };
 
 export async function getServerSideProps(context) {
     const { slug } = context.query;
     const res = await fetch(
         `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${slug.replace(
-            '-',
-            ' '
+            /-/g,
+            '%20'
         )}`
     );
-
     const data = await res.json();
     return { props: { data } };
 }
